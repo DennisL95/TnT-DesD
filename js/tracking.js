62f83c5c97cd696ef73f2ecdca1192368bc8f047
DennisL95 <104500006+DennisL95@users.noreply.github.com>
2022-09-29 15:18:48 +0200

    Update tracking.js

--- js/tracking.js
+++ js/tracking.js
@@ -1,5 +1,8 @@
 if (cookieConsent.trackingAllowed()) {
-    // vvv Google Analytics oder andere Tracking Codes hier einfügen vvv
+     if ($("#map").attr("src") == '') {
 $("#map").attr("src", 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2370.3696126356713!2d10.04164141394564!3d53.55116915813242!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5a0a7214b8744ec9!2sT%26T%20Design!5e0!3m2!1sen!2sde!4v1663669688707!5m2!1sen!2sde');
+}
+    // vvv Google Analytics oder andere Tracking Codes hier einfügen vvv
+
     //
 }
