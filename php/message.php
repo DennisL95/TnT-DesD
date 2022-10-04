51a1956f24e8a91fdbca2364a25a3198efe4f79d
DennisL95 <104500006+DennisL95@users.noreply.github.com>
2022-10-04 16:19:49 +0200

    Add files via upload

---
+++ php/message.php
@@ -0,0 +1,23 @@
+<?php
+  $name = htmlspecialchars($_POST['name']);
+  $email = htmlspecialchars($_POST['email']);
+  $message = htmlspecialchars($_POST['message']);
+
+  if(!empty($email) && !empty($message)){
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+      $receiver = "dennis_lin@web.de"; //enter that email address where you want to receive all messages
+      $subject = "From: $name <$email>";
+      $body = "Name: $name\nEmail: $email\n\nNachricht:\n$message";
+      $sender = "From: $email";
+      if(mail($receiver, $subject, $body, $sender)){
+         echo "Nachricht wurde versandt!";
+      }else{
+         echo "Nachricht konnte nicht versandt werden.";
+      }
+    }else{
+      echo "Eine gültige Email-Adresse ist erforderlich.";
+    }
+  }else{
+    echo "Email und Nachricht erforderlich.";
+  }
+?>
